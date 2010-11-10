3beb4d2f803bdafc463a92ea9588e5c7189c6d4d
SagarPatel <17.sagar.patel@gmail.com>
2010-11-10 16:25:21 -0500

    Set up Asg3 folder

---
+++ OpenGLTest/Asg3/DragonModel.h
@@ -0,0 +1,1097 @@
+
+#include <windows.h>
+#include "glut.h"
+#include <math.h>
+#include <stdio.h>
+
+#include "QuadricObjects.h"
+ 
+
+const int segments = 346;
+
+float Pitch_counter = 0;
+float Yaw_counter = 0;
+float Roll_counter = 0;
+
+float MaxRotation = 45;
+
+
+
+
+class DragonModel : public QuadricObjects
+{
+public:
+
+	// 285 in main
+	// 61 in tail
+	// total of 346
+	
+	float TransformMatrixArray[segments-1][16];
+
+	int *ArmCounter;
+	
+
+	DragonModel(int *wired_or_shade,int *AC) : QuadricObjects()
+	{
+		// Initiliazation Code Here
+		SetWoS(wired_or_shade);
+
+		ArmCounter = AC;
+
+		//printf("armcounter: %d \n",*ArmCounter );
+
+	}
+
+
+		
+	void showReferenceAxis(void)
+							
+	{
+
+		 glBegin(GL_LINES);
+							// X axis red
+	      glColor3f(1, 0, 0);
+	      glVertex3f(0, 0, 0);
+	      glVertex3f(20, 0, 0);
+							// Y axis green
+	      glColor3f(0, 1, 0);
+	      glVertex3f(0, 0, 0);
+	      glVertex3f(0, 20, 0);
+							// Z axis blue
+	      glColor3f(0, 0, 1);
+	      glVertex3f(0, 0, 0);
+	      glVertex3f(0, 0, 20);
+		  glEnd();
+		
+	}
+
+
+
+	void drawCone(float BaseRadius, float Height)
+	{
+
+		drawCylindre(BaseRadius,0, Height,8,2);
+	}
+
+	void drawSpikes(float BaseRadius, float Height)
+	{
+		//Transform Cones to make spikes on back of dragon
+
+
+		glPushMatrix();
+		//glColor3f(0,0,0); //Black spikes
+			
+			// Gold
+			// Original Code: 255-215-0
+			//glColor3f(0.99609375, 0.83984375, 0);
+		// Moded gold
+		//glColor3f(0.99609375, 0.83984375, 0.45);
+
+		// Dark Green
+		// Original Code: 0-100-0
+		//glColor3f(0.0, 0.390625, 0.0);
+
+		//Manually tweaked value
+		glColor3f(0,0.25,0.05);
+
+		glScalef(1,1,4);
+
+		glRotatef(-90,1,0,0); // Rotation to dragon's back
+			
+		glTranslatef(0,-0.5,0);  
+		
+		glRotatef(45,0,1,0); //Rotate to side anlge
+
+		
+
+		drawCone(BaseRadius,Height);
+
+		glRotatef(-90,0,1,0);
+		drawCone(BaseRadius,Height);
+
+
+		glPopMatrix();
+
+			//Return to Body Color
+
+			glColor3f(0.15625, 0.54296875, 0.236328125);
+		
+
+
+	}
+
+
+
+	void drawBelly(void)
+	{
+
+		//Draw Belly/Underside
+			//Wheat
+			//Orignal Code : 245-222-179
+				glColor3f(0.95703125, 0.8671875, 0.69921875);
+
+			glPushMatrix();
+			glTranslatef(0,-1.0,0);
+			drawSphere(3.0,15,15);
+			glPopMatrix();
+
+			//Return to Body Color
+
+			glColor3f(0.15625, 0.54296875, 0.236328125);
+		
+	}
+
+	void drawBelly(float Radius)
+	{
+
+		//Draw Belly/Underside
+			//Wheat
+			//Orignal Code : 245-222-179
+				glColor3f(0.95703125, 0.8671875, 0.69921875);
+
+			glPushMatrix();
+			glTranslatef(0,-1.0,0);
+			drawSphere(Radius,15,15);
+			glPopMatrix();
+
+			//Return to Body Color
+
+			glColor3f(0.15625, 0.54296875, 0.236328125);
+		
+	}
+
+	void drawLeftWing(void)
+	{
+		glPushMatrix();
+
+		glTranslatef(0,2,0);
+
+	//	drawCylindre(float TopRadius, float BotRadius, float Height, int CylindreDetailRadius,int CylindreDetailHeight)
+		
+	//Orignal Wing code
+		glPushMatrix();
+		glRotatef(60,0,1,0);
+		glRotatef(-35,1,0,0);
+
+
+		//First part of wing, stuck to back of body
+		glPushMatrix();
+		glScalef(1,0.15,1);
+		drawCylindre(1, 3, 6, 5,3);
+		glPopMatrix();
+
+
+		//Second part of wing, widest
+
+		glTranslatef(0,0,5);
+		
+		glRotatef(15,0,1,0);
+		glRotatef(-25,1,0,0);
+		//glRotatef(180,0,0,1);
+		
+		glPushMatrix();
+		glScalef(1,0.15,1);
+		drawCylindre(3,5,7,5,3);
+		glPopMatrix();
+
+		//Last part of wing
+
+		
+		glTranslatef(0,0,6.75);
+
+		glRotatef(-25,1,0,0);
+		glScalef(1,0.15,1);
+		//showReferenceAxis();
+		drawCylindre(5,1,4,5,3);
+		
+		glPopMatrix();
+
+		glPopMatrix();
+
+
+
+	}
+
+	void drawWings(void)
+	{
+		
+		//showReferenceAxis();
+
+
+		//glColor3f(0,0.2,0);
+	glPushAttrib(GL_CURRENT_BIT);
+
+	//Draw Left Wing
+		drawLeftWing();
+
+	//Draw Right Wing
+		glScalef(-1,1,1); //Does mirror image
+		drawLeftWing();
+		
+	glPopAttrib();
+
+
+	}
+
+	
+
+	
+	void drawRightArm(void)
+	{
+
+		int	bicep_length = 8;
+		int forearm_length = 7;
+
+		glPushAttrib(GL_CURRENT_BIT);
+
+		glPushMatrix();
+		glRotatef(35,0,1,0);
+		drawCylindre(1,1,bicep_length);
+
+		glTranslatef(0,0,bicep_length);
+		
+
+		drawSphere(1.15,10,10);
+
+		glRotatef(130,0 ,1,0);
+		drawCylindre(1,1,forearm_length);
+
+		glTranslatef(0,0,forearm_length);
+
+	//Draw hand
+		glPushMatrix();
+
+		//showReferenceAxis();
+		glScalef(1.1,1.1,1.5);
+		drawSphere(1.2,10,10);
+
+		glPopMatrix();
+
+	//Draw fingers
+		
+		glScalef(1.15,1.15,1.15);
+
+		glColor3f(0,0,0);
+		
+		glTranslatef(0.15,0,0.1);
+
+		glPushMatrix();
+		glRotatef(60,0,1,0);
+		drawCylindre(1,0,2);
+		glPopMatrix();
+
+		glTranslatef(0,-0.4,0.5);
+		glRotatef(30,1,0,0);
+		drawCylindre(0.75,0,1.5);
+
+		glTranslatef(-0.5,0,0.2);
+		glRotatef(-30,0,1,0);
+		glTranslatef(0,0.2,-0.5);
+		drawCylindre(1,0,1.5);
+
+		glPopMatrix();
+		
+
+		glPopAttrib();
+
+	}
+
+
+	void drawArms(void)
+	{
+
+		drawRightArm();
+		
+		glPushMatrix();
+
+		glScalef(-1,1,1);
+		drawRightArm();
+
+		glPopMatrix();
+
+	}
+
+	void drawLeftLeg(void)
+	{
+		
+		int	bicep_length = 7;
+		int forearm_length = 5;
+
+		glPushAttrib(GL_CURRENT_BIT);
+
+		glPushMatrix();
+		glTranslatef(2,0,0);
+		glRotatef(111,0,1,0);
+		glRotatef(-45,0,0,1);
+		//showReferenceAxis();
+		drawCylindre(1,1,bicep_length);
+
+		glTranslatef(0,0,bicep_length);
+		
+
+		drawSphere(1.15,10,10);
+
+		glRotatef(100,0 ,1,0); // joint angle
+		drawCylindre(1,1,forearm_length);
+
+		glTranslatef(0,0,forearm_length);
+
+	//Draw hand
+		glPushMatrix();
+
+		//showReferenceAxis();
+		glScalef(1.1,1.1,1.5);
+		drawSphere(1.2,10,10);
+
+		glPopMatrix();
+
+	//Draw fingers
+		
+		glScalef(1.15,1.15,1.15);
+
+		glColor3f(0,0,0);
+		
+		glTranslatef(0.15,0,0.1);
+
+		glPushMatrix();
+		glRotatef(60,0,1,0);
+		drawCylindre(1,0,2);
+		glPopMatrix();
+
+		glTranslatef(0,-0.4,0.5);
+		glRotatef(30,1,0,0);
+		drawCylindre(0.75,0,1.5);
+
+		glTranslatef(-0.5,0,0.2);
+		glRotatef(-30,0,1,0);
+		glTranslatef(0,0.2,-0.5);
+		drawCylindre(1,0,1.5);
+
+		glPopMatrix();
+		
+
+		glPopAttrib();
+
+
+
+	}
+
+	void drawLegs(void)
+	{
+
+		drawLeftLeg();
+
+		glPushMatrix();
+
+		glScalef(-1,1,1);
+
+		drawLeftLeg();
+
+		glPopMatrix();
+		
+
+	}
+
+
+	void drawHead(void)
+	{
+			
+		
+		glPushMatrix();
+
+		glTranslatef(0,0,3);
+
+	//Head Code
+
+		glPushMatrix();
+
+		glTranslatef(0,0,-2);
+		drawCylindre(5,1.20,9.3,20,20);
+
+		glPopMatrix();
+
+	//Mouth Code
+
+	glColor3f(0, 0.4, 0);
+
+		glPushMatrix();
+		glTranslatef(0,0,5);
+		glRotatef(15,1,0,0);
+		//glColor3f(0,0,1);
+		glScalef(0.75,0.75,1);
+		drawSphere(3.2,20,20);
+		glPopMatrix();
+
+
+
+	//Eyes code
+
+		//showReferenceAxis();
+
+		glColor3f(0.5,0,0);
+		glTranslatef(0,3,2);
+		
+
+		glPushMatrix();
+		glTranslatef(-2,0,0);
+		glRotatef(-40,0,1,0);
+		glScalef(1,0.4,0.5);
+		//showReferenceAxis();
+		drawSphere(1.5,10,10);
+		glPopMatrix();
+
+		glPushMatrix();
+		glTranslatef(2,0,0);
+		glRotatef(40,0,1,0);
+		glScalef(1,0.4,0.5);
+		//showReferenceAxis();
+		drawSphere(1.5,10,10);
+		glPopMatrix();
+
+
+
+	//Ears code
+
+		//showReferenceAxis();
+
+		glColor3f(0,0.1,0);
+		glTranslatef(0,-2,-3);
+		
+
+		glPushMatrix();
+		glTranslatef(-4,0,0);
+		glRotatef(-40,0,1,0);
+		glScalef(0.5,1,0.2);
+		//showReferenceAxis();
+		drawSphere(3,10,10);
+		glPopMatrix();
+
+		glPushMatrix();
+		glTranslatef(4,0,0);
+		glRotatef(40,0,1,0);
+		glScalef(0.5,1,0.2);
+		//showReferenceAxis();
+		drawSphere(3,10,10);
+		glPopMatrix();
+
+
+		glPopMatrix();
+
+	}
+
+
+
+
+//284 segments
+
+	void drawSegment(int counter)
+	{
+		//	showReferenceAxis();
+
+		if(counter%5 == 0)
+		{
+			drawCylindre(3.0,1.5,5);
+			drawBelly();
+			drawSpikes(1,4);
+		}
+		if(counter%5 == 1)
+		{
+			drawCylindre(3.0,1.5,5);
+			drawBelly();
+			drawSpikes(1,4.1);
+		}
+		if(counter%5 == 2)
+		{
+			drawCylindre(3.0,1.5,5);
+			drawBelly();
+			drawSpikes(1,4.2);
+		}
+		if(counter%5 == 3)
+		{
+			drawCylindre(3.0,1.5,5);
+			drawBelly();
+			drawSpikes(1,4.3);
+		}
+		if(counter%5 == 4)
+		{
+			drawCylindre(3.0,1.5,5);
+			drawBelly();
+			drawSpikes(1,4.4);
+		}
+		
+	}
+
+	void printMatrix(int row)
+	{
+		for(int i = 0;i<16;i++)
+		{
+			if(i%4==0)
+			{
+				printf("\n");
+			}
+
+			printf("%f  ",TransformMatrixArray[row][i]);
+			
+			
+		}
+		printf("\n");
+
+
+	}
+
+
+	void generateBody(void)
+	{
+
+		glPushAttrib(GL_CURRENT_BIT);
+
+		glPushMatrix();
+
+
+		glRotatef(90,0,1,0);
+		glRotatef(-90,1,0,0);
+		
+
+		int height_increment;
+
+		for(int i =0; i<285;i++)
+		{
+
+			if(i==1)
+			{
+				//drawLegs();
+			}
+			
+			if(i>1 && i<=20)
+			{
+				height_increment = i;
+				glTranslatef(0 , 0 , 1.5);
+				glRotatef(-0.2*height_increment,1,0,0);
+				glRotatef(2,0,1,0);
+				glRotatef(2,0,0,1);
+			}		
+
+			
+
+			if (i>20 && i<=40)
+			{
+				height_increment = 20;
+				glTranslatef(0 , 0 , 1.5);
+				glRotatef(-0.2*height_increment,1,0,0);
+				glRotatef(2,0,1,0);
+				glRotatef(2,0,0,1);
+			}
+			if (i>40 && i<=100)
+			{
+				height_increment = 40;
+				glTranslatef(0 , 0 , 1.5);
+				glRotatef(-0.2*height_increment,1,0,0);
+				glRotatef(2,0,1,0);
+				glRotatef(2,0,0,1);
+			}
+
+		
+			if(i<50)
+			{
+			//	drawSpikes(1,4);
+			}
+			if(i>=50)
+			{
+				
+			//	drawSpikes(1,4.5);
+			}
+			
+			
+			if(i>100 && i<=120)
+			{
+				height_increment = i-100;
+				glTranslatef(0 , 0 , 1.5);
+				glRotatef(0.235*height_increment,1,0,0);
+				glRotatef(2,0,1,0);
+				glRotatef(2,0,0,1);
+
+			}
+
+			if (i>120 && i<150)
+			{
+				height_increment = 20;
+				glTranslatef(0 , 0 , 1.5);
+				glRotatef(0.235*height_increment,1,0,0);
+				glRotatef(2,0,1,0);
+				glRotatef(2,0,0,1);
+			}
+		
+
+			if(i>=150 && i<=160)
+			{
+				height_increment = i-150;
+				glTranslatef(0 , 0 , 1.5);
+				glRotatef(0.2*height_increment,1,0,0);
+				glRotatef(2,0,1,0);
+				glRotatef(4,0,0,1);
+			}
+
+			if (i>160 && i<200)
+			{
+				height_increment = 40;
+				glTranslatef(0 , 0 , 1.5);
+				glRotatef(0.2*height_increment,1,0,0);
+				glRotatef(2,0,1,0);
+				glRotatef(4,0,0,1);
+			}
+
+
+			if(i>=200 && i ==201)
+			{
+				height_increment = i-200;
+				glTranslatef(0 , 0 , 1.5);
+				glRotatef(-0.2*height_increment,1,0,0);
+				//glRotatef(2,0,1,0);
+				glRotatef(4,0,0,1);
+			}
+
+			if (i>201 && i<230)
+			{
+				height_increment = 40;
+				glTranslatef(0 , 0 , 1.5);
+				glRotatef(-0.2*height_increment,1,0,0);
+				//glRotatef(2,0,1,0);
+				glRotatef(4,0,0,1);
+			}
+
+
+			if(i>=230 && i <241)
+			{
+				glTranslatef(0 , 0 , 1.5);
+			}
+
+
+			if(i>=241 && i<255)
+			{
+				glTranslatef(0 , 0 , 1.5);
+				
+				glRotatef(4,1,0,0);//x
+				glRotatef(4.5,0,1,0);//y
+				glRotatef(3.5,0,0,1);//z
+			}
+			
+
+//255
+
+			if(i>=255 && i<270)
+			{
+				glTranslatef(0 , 0 , 1.5);
+				glRotatef(-10,1,0,0);
+			}
+
+
+			if(i>=270 && i<284)
+			{
+				glTranslatef(0 , 0 , 1.5);
+				glRotatef(15,1,0,0);
+			}
+
+
+		if(i==284)
+		{
+			glTranslatef(0 , 0 , 1.5);
+			glRotatef(-15,1,0,0);
+		}
+
+
+	
+		glGetFloatv(GL_MODELVIEW_MATRIX , TransformMatrixArray[segments - 1 -i-61]);			
+	 
+	  }
+		
+		glPopMatrix();
+		glPopAttrib();
+		
+	 
+	}
+
+
+
+	void generateTail(void)
+	{
+
+		glPushAttrib(GL_CURRENT_BIT);
+
+		glPushMatrix();
+
+		showReferenceAxis();
+
+		glRotatef(90,0,1,0);
+		glRotatef(-90,1,0,0);
+
+
+		glTranslatef(0,0,1);
+
+		int height_increment;
+
+		for(int i = 285; i<=segments; i++)
+		{
+
+			
+
+		//	printf("%d \n",i);
+			
+
+			height_increment = i - 285;
+
+			glTranslatef(0 , 0 , -1.5);
+			//glRotatef(-0.2*height_increment,-1,0,0);
+			
+			if (i <30+285)
+			{
+				glRotatef(4,1,0,0);
+				glRotatef(-0.5*height_increment,0,1,0);
+				
+			}
+
+			if (i>=30+285 && i < 45+285)
+			{
+				glRotatef(4,0,1,0);
+				glRotatef(-9,1,0,0);
+				glRotatef(4,0,0,1);
+
+			}
+
+
+			if (i>=45+285)
+			{
+				glRotatef(4,0,0,1);
+				glRotatef(7,1,0,0);
+
+			}
+
+			// Transformations for main tail end here
+
+			// Save current matric into seperate array
+
+		glGetFloatv(GL_MODELVIEW_MATRIX , TransformMatrixArray[i]);
+
+		//printMatrix(i);
+			//printf("%f \n", TransformMatrixArray[1][1]);
+		}
+		glPopMatrix();
+
+		glPopAttrib();
+
+	}
+
+	void animateDragon(float *pitch, float *yaw, float *roll, float dist)
+	{
+
+
+		//printMatrix(0);
+	
+		//Move up values
+		if (dist>0)
+		{
+			for(int i=segments; i>0; i--)
+			{
+				if(i==0)
+				{
+					printf("oh noes");
+				}
+
+				// if i==0, means transfering first
+				for(int j=0; j<16; j++)
+				{
+							
+					TransformMatrixArray[i][j] = TransformMatrixArray[i-1][j];
+					//printf("%f \n",TransformMatrixArray[i][j]);
+				}
+
+			}
+		}
+
+
+
+
+		float Pitch = *pitch;
+		float Yaw = *yaw;
+		float Roll = *roll;
+
+
+		if (dist==0)
+		{
+			Pitch_counter += Pitch;
+			Yaw_counter += Yaw;
+			Roll_counter += Roll;
+		}
+
+		if(Pitch_counter>MaxRotation)
+		{
+			Pitch=0;
+			Pitch_counter=MaxRotation;	
+		}
+
+
+		if(Pitch_counter<-MaxRotation)
+		{
+			Pitch=0;
+			Pitch_counter=-MaxRotation;		
+		}
+	
+
+		if(Yaw_counter>MaxRotation)
+		{
+			Yaw=0;
+			Yaw_counter=MaxRotation;	
+		}
+		
+
+		if(Yaw_counter<-MaxRotation)
+		{
+			Yaw=0;
+			Yaw_counter=-MaxRotation;	
+		}
+
+
+
+		if(Roll_counter>MaxRotation)
+		{
+			Roll=0;
+			Roll_counter=MaxRotation;	
+		}
+		
+
+		if(Roll_counter<-MaxRotation)
+		{
+			Roll=0;
+			Roll_counter=-MaxRotation;	
+		}
+
+
+
+
+		 // printf("Pitch: %f    Yaw: %f   Roll: %f \n", Pitch, Yaw, Roll);
+		 // printf("PitchCounter: %f \n\n",Pitch_counter);
+
+	// Fix value of first element to reflect transformation
+
+		glPushMatrix();
+
+		glLoadMatrixf(TransformMatrixArray[0]);
+		
+		//Transformations to the first coordinate
+		
+		glTranslatef(0,0,dist);
+		
+		glRotatef(Pitch,1,0,0); // Pitch
+		glRotatef(Yaw,0,1,0); // Yaw
+		glRotatef(Roll,0,0,1); // Roll
+		
+		glScalef(1,1,1);
+		//Dumped transformed into first position
+		glGetFloatv(GL_MODELVIEW_MATRIX , TransformMatrixArray[0]);
+
+
+		glPopMatrix();
+
+		printMatrix(0);
+
+		
+
+	}
+
+	void updateCamera(void)
+	{
+		
+
+		// gluLookAt(130,130,130,
+		// 	  	  0,30,0,
+		// 	  	  0,1,0);
+
+		float CameraTarget[16];
+
+		glPushMatrix();
+
+		glLoadMatrixf(TransformMatrixArray[0]);
+		
+		glTranslatef(0,0,10);
+
+		glGetFloatv(GL_MODELVIEW_MATRIX , CameraTarget);
+
+		glPopMatrix();
+
+
+
+		gluLookAt(TransformMatrixArray[0][12],TransformMatrixArray[0][13],TransformMatrixArray[0][14],
+			  	  CameraTarget[12],CameraTarget[13],CameraTarget[14],
+			  	  0,1,0);
+		
+
+	}
+
+
+	void drawDragon(void)
+	{
+		
+
+
+		showReferenceAxis();
+
+		// Orignal Color (hand tweaked)
+		//	glColor3f(0.15, .75, 0.5);
+
+		// Forest Green (http://www.tayloredmktg.com/rgb/)
+		// Original code : 34-139-34
+			glColor3f(0.1328125, 0.54296875, 0.1328125);
+
+		// Sea Green
+		// Original Code : 46-139-87
+		//	glColor3f(0.1796875, 0.54296875, 0.33984375);
+
+		//Average of Forest and Sea
+	//		glColor3f(0.15625, 0.54296875, 0.236328125);
+
+		
+
+		for(int i=0; i<segments; i++)
+		{
+			glPushMatrix();
+
+
+		//	glLoadMatrixf( TransformMatrixArray[i] ); //Loading makes it always at the same place relative to eye
+
+			glMultMatrixf(TransformMatrixArray[i]);
+
+		
+			drawSegment(i);
+
+
+//ARMS		
+			
+			//printf("armcoutner %d \n",*ArmCounter);
+
+			float rot = 0;
+
+			//rot = *ArmCounter;
+
+			if(i==290)
+			{
+
+				glPushMatrix();
+				glRotatef(rot,0,0,0);
+				drawArms();
+				glPopMatrix();
+			}
+
+			if(i==10)
+			{
+
+				glPushMatrix();
+				glRotatef(rot,0,0,0);
+				glRotatef(-80,1,0,0);
+				glTranslatef(0,0,-2);
+				drawArms();
+				glPopMatrix();
+
+			}
+
+			if(i==24)
+			{
+				drawWings();
+			}
+
+
+			if(i==0)
+			{
+				
+				drawHead();
+			}
+
+
+//Tail
+			if(i==segments-1)
+			{
+
+				glPushMatrix();
+				
+				glTranslatef(0,-4,5);
+				glScalef(1.75,2,1);
+
+				glPushMatrix();
+
+				for(int j =0;j<7;j++)
+				{
+					glTranslatef(0,0.2,-1.5);
+					drawSpikes(2,4.5);
+				}
+
+				glPopMatrix();
+
+				glTranslatef(0,1,0);
+				glScalef(1.25,-0.25,1.25);
+
+				for(int k =0;k<3;k++)
+				{
+					glTranslatef(0,0.2,-1.5);
+					drawSpikes(2,4);
+				}
+
+				glPopMatrix();
+				
+			}
+
+
+			glPopMatrix();
+
+		}
+
+
+	}
+
+
+
+
+
+
+		
+	
+
+	void drawTrees(void)
+	{
+
+		int tree_height = 10;
+		int tree_thickness = 2;
+
+		glPushMatrix();
+		glPushAttrib(GL_CURRENT_BIT);
+
+
+		// Original Code : 102,58,25
+		// Coverted = 0.3984375, 0.226525, 0.09765625
+		glColor3f(0.3984375, 0.226525, 0.09765625);
+		
+
+		glTranslatef(10,0,10);
+
+		glRotatef(-90,1,0,0);
+
+		//showReferenceAxis();
+		drawCylindre(tree_thickness,tree_thickness,tree_height);
+
+
+		glTranslatef(0,0,tree_height+5);
+
+		//Autumn Color http://www.perbang.dk/rgb/FF5700/
+		// Original Code: 255,87,0
+		// Converted = 1, 0.33984375, 0
+		glColor3f(1, 0.233984375, .05);
+
+		drawSphere(8.0,15,15);
+
+		glPopAttrib();
+		glPopMatrix();
+		
+	}
+
+
+	
+
+
+};
---
+++ OpenGLTest/Asg3/dragon3.cpp
@@ -0,0 +1,527 @@
+// COMP 371 Asg1
+// Sagar Patel 9356037
+
+// This code uses parts of pilotView.c sample provided in the Lab.
+// This usage is mainly limited to camera fucntions.
+
+#include <windows.h>
+#include "glut.h"
+#include <math.h>
+#include <stdio.h>
+#include "SOIL.h"
+
+//#include "QuadricObjects.h"
+#include "DragonModel.h"
+
+#define PI 3.14159265
+
+GLdouble cam_position[3];	
+GLdouble cam_target[3];	
+GLdouble cam_up[3];	
+
+
+GLuint tex_2D;
+
+
+double cam_theta;
+double cam_phi;
+double cam_radius;
+
+int h,w;
+
+int Wired_or_Shade = GLU_LINE;
+
+int ArmCounter = 10;
+
+bool orthogonalFlag = false;
+
+bool cameraFlag = false; // false == TP true == FP
+
+float zoom = 0.95;
+
+
+float Pitch = 0;
+float Yaw = 0;
+float Roll = 0;
+
+// Function Prototypes
+void init(void);
+void showReferenceAxis(void);
+
+
+void display(void);
+void reshape (int w, int h);
+void keyboard(unsigned char key, int x, int y);
+void specialCallbackProc (int key, int x, int y);
+
+
+
+
+
+//QuadricObjects quadricobjects(&Wired_or_Shade);
+DragonModel dragonmodel(&Wired_or_Shade,&ArmCounter);
+
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode (GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize (800, 800); 
+	glutInitWindowPosition (100, 100);
+	glutCreateWindow (argv[0]);
+
+	init ();
+
+	
+
+	glutDisplayFunc(display); 
+	
+
+	glutKeyboardFunc(keyboard);
+	glutSpecialFunc(specialCallbackProc);
+
+	glutReshapeFunc(reshape);
+
+	glutMainLoop();
+	return 0;
+}
+
+
+
+
+void init(void) 
+{
+
+	
+	dragonmodel.generateBody();
+	dragonmodel.generateTail();
+	// Shift values just once
+	dragonmodel.animateDragon(&Pitch,&Yaw,&Roll,1.5);
+
+	tex_2D = SOIL_load_OGL_texture
+    (
+        "dragonscales.jpg",
+        SOIL_LOAD_RGBA,
+        SOIL_CREATE_NEW_ID,
+        SOIL_FLAG_NTSC_SAFE_RGB
+    );
+
+	cam_radius = 100;
+
+	// Setting cam position to origin
+	// It is irreleveant since radius has been set to 10,
+	// therefore, thses values will be recalculated later.
+	cam_position[0] = 30;
+	cam_position[1] = 30;
+	cam_position[2] = 30;
+
+	// Point camera to center of dragon
+	cam_target[0] = 0;
+	cam_target[1] = 40;
+	cam_target[2] = 0;
+
+	// Setting camera's Up vector
+	// In this case, Up is the +Yaxis
+	cam_up[0] = 0;
+	cam_up[1] = 1;
+	cam_up[2] = 0;
+
+	cam_phi = 0.69;
+	cam_theta = -3.99;
+
+	glMatrixMode(GL_PROJECTION);
+								
+	glLoadIdentity();
+								
+	gluPerspective(90*zoom, (GLfloat)w/(GLfloat)h, 1.0, 10000.0);
+
+	glMatrixMode(GL_MODELVIEW);	
+								
+	glLoadIdentity();
+								
+	//Background Color
+		//Cornflower Blue
+		//Original Code: 100-149-237
+	//glClearColor (0.390625, 0.58203125, 0.92578125, 0.0);
+	glClearColor(0,0,0,0);
+	 
+	glEnable(GL_DEPTH_TEST);
+	glShadeModel (GL_SMOOTH);
+
+	
+	//Sets lighting
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	// Allows color on models with lighting
+	glEnable(GL_COLOR_MATERIAL);
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+	
+	//dragonmodel.generateTail();
+
+}
+
+
+
+
+
+void drawFloor(void)
+	{
+		glBindTexture(GL_TEXTURE_2D, tex_2D);
+
+		glBegin(GL_QUADS);					
+			glTexCoord2f(1.0f, 1.0f); 
+			glVertex3f(400, 0,  400);	
+			
+			glTexCoord2f(1.0f, 0.0f); 
+			glVertex3f( 400, 0,  -400);	
+			
+			glTexCoord2f(0.0f, 0.0f); 
+			glVertex3f(-400, 0,  -400);
+
+			glTexCoord2f(0.0f, 1.0f); 
+			glVertex3f( -400,  0,  400);
+
+		glEnd();
+	}
+
+
+
+void display(void)
+{
+
+	//135-206-250
+
+	glClearColor(0.52734375,0.8046875,0.9765625,0);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+
+		glMatrixMode(GL_MODELVIEW);
+	
+
+	glLoadIdentity();
+
+
+
+
+	cam_position[0] = cam_radius * sin(cam_phi) * sin(cam_theta);
+	cam_position[1] = cam_radius * cos(cam_phi);
+	cam_position[2] = cam_radius * sin(cam_phi) * cos(cam_theta);
+	
+	
+	if(cameraFlag == false)
+	{
+
+		gluLookAt(cam_position[0],cam_position[1],cam_position[2],
+				  cam_target[0], cam_target[1], cam_target[2],
+				  cam_up[0], cam_up[1], cam_up[2]);
+	}
+
+	if(cameraFlag == true)
+	{
+		dragonmodel.updateCamera();
+	}
+
+	drawFloor();
+
+	glPushMatrix();
+	glScalef(0.5,0.5,0.5);
+
+	dragonmodel.drawTrees();
+
+	glPushMatrix();
+	glTranslatef(170,0,110);
+	dragonmodel.drawTrees();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(110,0,180);
+	dragonmodel.drawTrees();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glTranslatef(105,0,-110);
+	dragonmodel.drawTrees();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glTranslatef(180,0,-190);
+	dragonmodel.drawTrees();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-105,0,180);
+	dragonmodel.drawTrees();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glTranslatef(-180,0,190);
+	dragonmodel.drawTrees();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glTranslatef(-195,0,-110);
+	dragonmodel.drawTrees();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glTranslatef(-110,0,-170);
+	dragonmodel.drawTrees();
+	glPopMatrix();
+
+
+
+	glPushMatrix();
+	glTranslatef(10,0,110);
+	dragonmodel.drawTrees();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glTranslatef(350,0,-110);
+	dragonmodel.drawTrees();
+	glPopMatrix();
+
+
+
+	glPushMatrix();
+	glTranslatef(-300,0,210);
+	dragonmodel.drawTrees();
+	glPopMatrix();
+
+
+
+	glPushMatrix();
+	glTranslatef(-300,0,-310);
+	dragonmodel.drawTrees();
+	glPopMatrix();
+
+
+
+	glPopMatrix();
+
+
+//Psuh Dragon up
+	glPushMatrix();
+
+	glTranslatef(0,20,0);
+	
+	dragonmodel.drawDragon();
+
+
+
+	glPopMatrix();
+
+
+	glutSwapBuffers();
+
+
+}
+
+void reshape (int w, int h)
+{
+	glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity ();
+	gluPerspective(90*zoom, (GLfloat) w/(GLfloat) h, 1.0, 800.0);
+	glMatrixMode (GL_MODELVIEW);
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+
+	
+	switch (key) 
+	{
+
+		case '1':
+			cameraFlag = true;
+			break;
+
+		case '3':
+			cameraFlag = false;
+			break;
+
+		case 'r':
+			glLoadIdentity();
+			dragonmodel.generateBody();
+			dragonmodel.generateTail();
+
+
+		case 'k':
+			Pitch += 1;
+			Yaw = 0;
+			Roll = 0;
+			if(Pitch>90){Pitch = 90;}
+			if(Pitch<0){Pitch=0;}
+			dragonmodel.animateDragon(&Pitch,&Yaw,&Roll,0);
+			break;
+		
+		case 'i':
+			Pitch -= 1;
+			Yaw = 0;
+			Roll = 0;
+			if(Pitch<-90){Pitch = -90;}
+			if(Pitch>0){Pitch =0;}
+			dragonmodel.animateDragon(&Pitch,&Yaw,&Roll,0);
+			break;
+
+		case 'j':
+			Yaw += 1;
+			Pitch = 0;
+			Roll = 0;
+			if(Yaw>90){Yaw = 90;}
+			if(Yaw<0){Yaw=0;}
+			dragonmodel.animateDragon(&Pitch,&Yaw,&Roll,0);
+			break;
+		
+		case 'l':
+			Yaw -= 1;
+			Pitch = 0;
+			Roll = 0;
+			if(Yaw<-90){Yaw = -90;}
+			if(Yaw>0){Yaw =0;}
+			dragonmodel.animateDragon(&Pitch,&Yaw,&Roll,0);
+			break;
+
+		case 'u':
+			Roll += 1;
+			Yaw = 0;
+			Pitch =0;
+			if(Roll>90){Roll = 90;}
+			if(Roll<0){Roll=0;}
+			dragonmodel.animateDragon(&Pitch,&Yaw,&Roll,0);
+			break;
+		
+		case 'o':
+			Roll -= 1;
+			Yaw = 0;
+			Pitch =0;
+			if(Roll<-90){Roll = -90;}
+			if(Roll>0){Roll =0;}
+			dragonmodel.animateDragon(&Pitch,&Yaw,&Roll,0);
+			break;
+
+		case 'z':
+			zoom -= 0.01;
+			//printf("%f \n",zoom);
+			break;
+		
+		case 'Z':
+			zoom += 0.01;
+			break;
+
+
+		case 'a':
+			dragonmodel.animateDragon(&Pitch,&Yaw,&Roll,1.5);
+			break;
+
+		case 'f':				// Move Camera forward.
+			cam_radius -= 0.5;
+			break;
+
+		case 'b' :				// Move Camera backward.
+			cam_radius += 0.5;
+			break;
+
+		case 'w':
+			Wired_or_Shade = GLU_FILL;
+			break;
+
+		case 'W':
+			Wired_or_Shade = GLU_LINE;
+			break;
+
+		case 's':
+			Wired_or_Shade = GLU_SILHOUETTE;
+			break;
+
+		case 'S':
+			Wired_or_Shade = GLU_POINT;
+			break;
+		
+
+		case 'c':
+			cam_radius = 100;
+			cam_phi = 0.69;
+			cam_theta = -3.99;
+		
+		break;
+
+		case 'C':	
+			cam_radius = 100;
+			cam_phi = 0.69;
+			cam_theta = -3.99;
+		
+		break;
+
+
+		case 27: // ESC key
+		exit(0);
+		break; 
+	}
+
+	
+
+	glutPostRedisplay();
+}
+
+
+void specialCallbackProc (int key, int x, int y)
+					
+{
+	switch (key)		
+	{
+		
+		case GLUT_KEY_LEFT:		// Rotate Camera in an anticlockwise direction about the Y axis of the At (center) point.
+			cam_theta -= 1.0 * (PI/180);
+			
+
+			break;
+
+		case GLUT_KEY_RIGHT:	// Rotate Camera in an clockwise direction about the Y axis of the At (center) point.
+			cam_theta += 1.0 * (PI/180);
+			break;
+
+		case GLUT_KEY_UP :		// Rotate Camera in an anticlockwise direction about the X axis of the At (center) point.
+			cam_phi -= 1.0 * (PI/180);
+			break;
+
+		case GLUT_KEY_DOWN :	// Rotate Camera in an clockwise direction about the X axis of the At (center) point.
+			cam_phi += 1.0 * (PI/180);
+			break;
+	}
+
+	glutPostRedisplay();
+}
+
+
+
+void showReferenceAxis(void)
+							
+	{
+
+		 glBegin(GL_LINES);
+							// X axis red
+	      glColor3f(1, 0, 0);
+	      glVertex3f(0, 0, 0);
+	      glVertex3f(20, 0, 0);
+							// Y axis green
+	      glColor3f(0, 1, 0);
+	      glVertex3f(0, 0, 0);
+	      glVertex3f(0, 20, 0);
+							// Z axis blue
+	      glColor3f(0, 0, 1);
+	      glVertex3f(0, 0, 0);
+	      glVertex3f(0, 0, 20);
+		  glEnd();
+		
+	}
+
